0ef7dc69d1878b8265cf22a742b6e7afef637ca2
Isaac Miller <imick5555@gmail.com>
2025-12-16 14:27:39 -0700

    updated package

--- src/final_project_demo/__init__.py
+++ src/final_project_demo/__init__.py
@@ -1,8 +1,24 @@
 from .analysis import run_analysis_pipeline, add
 from .cleaning import run_cleaning_pipeling
 
+from final_project_demo.gathering_salaries import (
+    add_one,
+    calculate_mean,
+    create_http_headers,
+    parse_salary_table_from_soup,
+    scrape_salary_from_url,
+    extract_unique_links,
+    scrape_with_cloudscraper,
+    churn_with_cloudscraper,
+)
+
 __all__ = [
-    "run_analysis_pipeline",
-    "add",
-    "run_cleaning_pipeling",
+    "add_one",
+    "calculate_mean",
+    "create_http_headers",
+    "parse_salary_table_from_soup",
+    "scrape_salary_from_url",
+    "extract_unique_links",
+    "scrape_with_cloudscraper",
+    "churn_with_cloudscraper",
 ]
